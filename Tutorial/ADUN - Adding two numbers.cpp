48c5afab6f127fae4768d7769f409234252565ea
Raghav Bhandari <raghavb50@gmail.com>
2017-05-13 15:23:14 +0530

    Create ADUN - Adding two numbers.cpp

---
+++ Tutorial/ADUN - Adding two numbers.cpp
@@ -0,0 +1,32 @@
+/*
+Your task is to read two numbers a and b (0 < a, b <2100000000) and to output their sum.
+
+Input
+
+Input contains two lines, on the first line the number a and on the second line the number b.
+
+Output
+
+Output the sum of the two numbers.
+
+Example
+
+Input:
+20
+30
+
+Output:
+50
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	cout<<a+b;
+	return 0;
+}

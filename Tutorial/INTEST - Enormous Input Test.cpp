d78e38d3bc2733026857d51ab2c7a92f2a31c417
Raghav Bhandari <raghavb50@gmail.com>
2017-05-13 15:02:15 +0530

    Create INTEST - Enormous Input Test.cpp

---
+++ Tutorial/INTEST - Enormous Input Test.cpp
@@ -0,0 +1,45 @@
+/*
+The purpose of this problem is to verify whether the method you are using to read input data is sufficiently fast to handle problems branded with the enormous Input/Output warning. You are expected to be able to process at least 2.5MB of input data per second at runtime.
+
+Input
+
+The input begins with two positive integers n k (n, k<=107). The next n lines of input contain one positive integer ti, not greater than 109, each.
+
+Output
+
+Write a single integer to output, denoting how many integers ti are divisible by k.
+
+Example
+
+Input:
+7 3
+1
+51
+966369
+7
+9
+999996
+11
+
+Output:
+4
+
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int n,k,t,count=0;
+	cin>>n;
+	cin>>k;
+	while(n--)
+	{
+		cin>>t;
+		if(t%k==0)
+		count++;
+	}
+	cout<<count;
+	return 0;
+}
